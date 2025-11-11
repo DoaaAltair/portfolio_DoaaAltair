dfa6211df9f3e4441fbdf7e43677e2e84511854d
DoaaAltair <doaa.altair-94@hotmail.com>
2025-11-11 12:00:29 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // Turbopack uitschakelen om de bekende bug te vermijden
-  experimental: {
-    turbo: false,
-  },
 };
 
 export default nextConfig;
